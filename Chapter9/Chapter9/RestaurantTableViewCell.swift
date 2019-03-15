75c1d33a7eae388ad16ce17a1a0b5abd2865d101
Nazih <nazih.altajar@fortech.ro>
2019-03-15 14:25:11 +0200

    Chapter9

---
+++ Chapter9/Chapter9/RestaurantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantTableViewCell.swift
+//  Chapter9
+//
+//  Created by Nazih Al Tajar on 15/03/2019.
+//  Copyright © 2019 sss. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+        thumbnailImageView.clipsToBounds = true
+    }
+}
